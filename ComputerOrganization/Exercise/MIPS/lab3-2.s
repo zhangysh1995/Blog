08c231bcceb1077e435289ac15bea65feb777d5f
zhangysh1995 <zhangysh1995@gmail.com>
2016-03-22 23:10:14 +0800

    Bubble Sort

--- ComputerOrganization/Exercise/MIPS/lab3-2.s
+++ ComputerOrganization/Exercise/MIPS/lab3-2.s
@@ -7,6 +7,8 @@ array:	.space	32
 msg:	.asciiz "\nInput an integer:\n"
 msg1:	.asciiz "\nSum of negative: "
 msg2:	.asciiz "\nSum of positive: "
+msg3:	.asciiz "\n"
+seprate:.asciiz ","
 
 	.text
 main:	la $t0,array
@@ -64,7 +66,47 @@ exit:		la $a0, msg2
 		move $a0, $s1
 		li $v0, 1
 		syscall			#print sum of negative
-		li $v0, 10
+		la $a0, msg3
+		li $v0, 4
 		syscall
+		
 # If you want to print ordered numbers first, just branchs the code when deciding negetive or positive. Therefore 
 # the code would be longer and more difficult to read.
+
+
+	#bubble sort the array
+
+ 	addi $t5,$zero,8     # size of arry
+ 	addi $t4,$zero,7     # intialize first loop counter i=10
+loop3:
+ 	la   $t1,array       # load base address of arry,t1 as the offset address of the count
+ 	addi $t6,$zero,0     # intialize second loop counter j=0
+loop4:
+ 	lw   $t0,0($t1)      # initialize t0=arry[j] as the temp value
+ 	lw   $t7,4($t1)      # t7=arry[j+1]
+ 	sub  $t2,$t0,$t7     # t2=arry(j+1)-temp
+ 	bgtz $t2,skip2      # if temp<arry(j+1),goto skip2
+    	sw   $t7,0($t1)      # arry[j]=arry[j+1]
+    	sw   $t0,4($t1)      # arry[j+1]=temp
+skip2:
+ 	addi $t6,$t6,1        #j=j+1 
+ 	addi $t1,$t1,4
+ 	sub  $t2,$t4,$t6      #t2=i-j
+ 	bgtz  $t2,loop4      #if(j<i) back to loop2
+
+ 	addiu $t4,$t4,-1        #i=i-1 
+ 	lw    $a0,0($t1)
+ 	li    $v0,1             #out put segment
+ 	syscall
+ 	la    $a0,seprate
+ 	li    $v0,4 
+ 	syscall
+ 	bne  $t4,$zero,loop3
+
+ 	la   $t1,array       #loop1 over,output the last number
+ 	lw    $a0,0($t1)
+ 	li    $v0,1
+ 	syscall
+
+ 	li   $v0,10
+ 	syscall
